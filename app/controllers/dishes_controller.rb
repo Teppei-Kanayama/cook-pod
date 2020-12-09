2161514ab24715270d2b233d0af96fecc66611af
Teppei-Kanayama <kanayama.teppei@gmail.com>
2020-12-09 12:40:27 +0000

    index page

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -1,8 +1,20 @@
 class DishesController < ApplicationController
+  def index
+  end
+  
   def show
     @dish = Dish.find(params[:id])
   end
   
   def new
+  end
+  
+  def create
+  end
+  
+  def edit
+  end
+  
+  def delete
   end
 end
