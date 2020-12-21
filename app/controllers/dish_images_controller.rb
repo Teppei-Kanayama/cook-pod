be8324dd5bb35b1f6a4e2069bfea89ec2a6494b3
Teppei-Kanayama <kanayama.teppei@gmail.com>
2020-12-21 12:44:39 +0000

    random filename

--- app/controllers/dish_images_controller.rb
+++ app/controllers/dish_images_controller.rb
@@ -4,9 +4,10 @@ class DishImagesController < ApplicationController
   end
   
   def create
-    @dish_image = DishImage.new(dish_image_params)
+    dish_id = params[:dish_image][:dish_id]
+    filename = ('a'..'z').to_a.shuffle[0..7].join
+    @dish_image = DishImage.new(dish_id: dish_id, filename: filename)
     if @dish_image.save
-      dish_id = params[:dish_image][:dish_id]
       dish = Dish.find_by_id(dish_id)
       redirect_to dish_url(dish)
     else
@@ -15,10 +16,10 @@ class DishImagesController < ApplicationController
     end
   end
   
-  private
+  # private
 
-    # マスアサインメント対策
-    def dish_image_params
-      params.require(:dish_image).permit(:dish_id, :filename)
-    end
+  #   # マスアサインメント対策
+  #   def dish_image_params
+  #     params.require(:dish_image).permit(:dish_id, :filename)
+  #   end
 end
