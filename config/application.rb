a28f949bb21bf8bafafb57e0a1e8a1444582bbdc
Teppei-Kanayama <kanayama.teppei@gmail.com>
2020-12-28 11:11:30 +0000

    make error messages japanese

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,10 @@ module CookPod
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+    
+    # These settings are thanks for https://qiita.com/Ushinji/items/242bfba84df7a5a67d5b
+    config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.yml').to_s]
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
